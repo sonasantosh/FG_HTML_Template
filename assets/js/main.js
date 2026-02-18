77135e6281ff4e1c9aabfba7e7f0db24789ce093
santosh kumar <skumar5@q3tech.com>
2026-02-18 23:43:23 +0530

    Carousel Added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,25 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        items: 2,
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 0,
+                margin: 0,
+            },
+            1024: {
+                items: 1,
+                stagePadding: 200,
+            },
+            1200: {
+                items: 2
+            },
+        }
+    })
 });
 
